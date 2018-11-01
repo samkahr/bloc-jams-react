44ed2416073e3f00d74e3c9a2056d142bf69912f
Sam Kahr <samkahr@gmail.com>
2018-11-01 12:56:11 +0400

    adding code from events checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+import Landing from './components/Landing';
+import Library from './components/Library';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <nav>
+                   <Link to='/'>Landing</Link>
+                   <br/>
+                   <Link to='/library'>Library</Link>
+          </nav>
+      <header>
+        <h1>Bloc Jams</h1>
+      </header>
+      <main>
+      <Route exact path="/" component={Landing} />
+      <Route path="/library" component={Library} />
+       </main>
       </div>
     );
   }
