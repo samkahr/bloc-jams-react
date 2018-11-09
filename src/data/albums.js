3a09da094db2132c1d861ccbb64253e0cc38eec5
samkahr <42845261+samkahr@users.noreply.github.com>
2018-11-09 17:43:16 +0400

    Update albums.js

--- src/data/albums.js
+++ src/data/albums.js
@@ -5,11 +5,11 @@ export default [{
   albumCover: '/assets/images/album_covers/01.jpg',
   slug: 'the-colors',
   songs: [
-      { songNumber: "1", title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-      { songNumber: "2", title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
-      { songNumber: "3", title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
-      { songNumber: "4", title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
-      { songNumber:  "5", title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
   ]
 }, {
     title: 'The Telephone',
@@ -18,10 +18,10 @@ export default [{
     albumCover: '/assets/images/album_covers/02.jpg',
     slug: 'the-telephone',
     songs: [
-      { songNumber: "1", title: 'Blue ', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-      { songNumber: "2", title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
-      { songNumber: "3", title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
-      { songNumber: "4", title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
-      { songNumber: "5", title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+      { title: 'Blue ', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
 }];
